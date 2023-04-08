3c3107f90c6087be2ad9b1e72a9871b31df9f26a
Nitish Goswami <devnitish97@gmail.com>
2023-04-08 16:45:03 +0530

    basic bit manipulation

---
+++ Bit Manipulation/Intro.java
@@ -0,0 +1,23 @@
+
+
+public class Intro {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+
+        System.out.println("OR : "+(x | y));
+        System.out.println("AND : "+(x & y));
+        System.out.println("XOR : "+(x ^ y));
+        System.out.println("~ : "+(~x));
+
+        int get3rdBit = ((x & 1<<4 )> 0) ? 1 : 0;
+        System.out.println(get3rdBit);
+
+
+        int set3rdBit = 2 | 1<<2;
+        System.out.println(set3rdBit);
+
+        int clear2Bit = 15 & ~(1<<2);
+        System.out.println(clear2Bit);
+    }
+}
